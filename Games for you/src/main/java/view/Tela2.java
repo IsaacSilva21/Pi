61bd9325a12074fc2db8584233409629c4ac01f1
IsaacSilva21 <125047197+IsaacSilva21@users.noreply.github.com>
2024-03-02 15:26:32 -0300

    Add files via upload

---
+++ Games for you/src/main/java/view/Tela2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package view;
+
+/**
+ *
+ * @author isaac
+ */
+public class Tela2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
