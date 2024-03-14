aab640105dfe6e614e26b216c7b763d91f276970
Matheus <117592662+Matheus21MSA@users.noreply.github.com>
2024-03-13 22:43:15 -0300

    Subindo Projeto

---
+++ GamesForYou3/GamesForYou/src/main/java/br/com/GamesForYou/GamesForYou/GamesForYouApplication.java
@@ -0,0 +1,13 @@
+package br.com.GamesForYou.GamesForYou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesForYouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesForYouApplication.class, args);
+	}
+
+}
